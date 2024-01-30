b519ca01c02607f2869e545e4a12cabaa10c7d41
Shr3ddin <patrykprzybylek94@gmail.com>
2024-01-30 02:37:28 +0100

    styling header, nav

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const burgerBtn = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 
 const handleNav = () => {
-	nav.classList.toggle('active');
+	nav.classList.toggle('nav--active');
 	burgerBtn.classList.toggle('is-active');
 };
 
