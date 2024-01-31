f6b386111769678242544f5d651e882cdb84b107
Shr3ddin <patrykprzybylek94@gmail.com>
2024-01-31 22:40:38 +0100

    implementing search btn

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 const burgerBtn = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 
+const searchBtn = document.querySelector('.search-btn');
+const searchForm = document.querySelector('.search-form');
+
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
 	burgerBtn.classList.toggle('is-active');
 };
 
+const handleSearch = () => {
+	searchForm.classList.toggle('search-form--active');
+};
+
 burgerBtn.addEventListener('click', handleNav);
+searchBtn.addEventListener('click', handleSearch);
