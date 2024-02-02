a22d91d642ca30034beff58c59d77c649fceecd5
Shr3ddin <patrykprzybylek94@gmail.com>
2024-02-02 01:45:32 +0100

    working on hero section design, hero slider logic added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ const nav = document.querySelector('.nav');
 const searchBtn = document.querySelector('.search-btn');
 const searchForm = document.querySelector('.search-form');
 
-console.log(searchForm);
+const sliderItems = document.querySelectorAll('.hero__item');
+
+let currentSlide = sliderItems[0];
+let currensSlidePos = 0;
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
@@ -19,5 +22,27 @@ const handleSearch = () => {
 	searchForm.classList.toggle('search-form--active');
 };
 
+const changeSlide = () => {
+	currentSlide.classList.remove('hero__item--active');
+	sliderItems[currensSlidePos].classList.add('hero__item--active');
+	currentSlide = sliderItems[currensSlidePos];
+};
+
+const nextSlide = () => {
+	if (currensSlidePos >= sliderItems.length - 1) {
+		currensSlidePos = 0;
+	} else {
+		currensSlidePos++;
+	}
+	changeSlide();
+};
+
+const autoChangeSlide = () => {
+	setInterval(() => {
+		nextSlide();
+	}, 7000);
+};
+
 burgerBtn.addEventListener('click', handleNav);
 searchBtn.addEventListener('click', handleSearch);
+window.addEventListener('load', autoChangeSlide);
