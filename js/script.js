9c44da808adbd6358519a028b68dde368ad00b9b
Shr3ddin <patrykprzybylek94@gmail.com>
2024-02-10 01:36:46 +0100

    working on bestsellers section styles and nav/header behavior

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const header = document.querySelector('.header');
+
 const burgerBtn = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 
@@ -9,8 +11,36 @@ const searchForm = document.querySelector('.search-form');
 
 const sliderItems = document.querySelectorAll('.hero__item');
 
+let lastScrollYPos = 0;
+
 let currentSlide = sliderItems[0];
 let currensSlidePos = 0;
+
+const hideHeader = () => {
+	const isScrollBottom = lastScrollYPos < window.scrollY;
+
+	if (isScrollBottom) {
+		header.classList.add('header--hide');
+	} else {
+		header.classList.remove('header--hide');
+	}
+
+	if (header.classList.contains('header--hide') && nav.classList.contains('nav--active')) {
+		nav.classList.remove('nav--active');
+		burgerBtn.classList.remove('is-active');
+	}
+
+	lastScrollYPos = window.scrollY;
+};
+
+const handleHeader = () => {
+	if (window.scrollY > 50) {
+		header.classList.add('header--active');
+		hideHeader();
+	} else {
+		header.classList.remove('header--active');
+	}
+};
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
@@ -59,6 +89,7 @@ const autoChangeSlide = () => {
 	}, 7000);
 };
 
+window.addEventListener('scroll', handleHeader);
 burgerBtn.addEventListener('click', handleNav);
 searchBtn.addEventListener('click', handleSearch);
 window.addEventListener('load', autoChangeSlide);
